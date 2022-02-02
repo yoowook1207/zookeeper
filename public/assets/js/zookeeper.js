c1b4a58168b20f93ecdb7dcab5ef71eb415916eb
Sung Yoo <yoowook1207@gmail.com>
2022-02-02 16:27:14 -0500

    module 4.8 done

--- public/assets/js/zookeeper.js
+++ public/assets/js/zookeeper.js
@@ -1,3 +1,4 @@
+const $zookeeperForm = document.querySelector('#zookeeper-form');
 const $displayArea = document.querySelector('#display-area');
 
 const printResults = resultArr => {
@@ -21,6 +22,12 @@ const printResults = resultArr => {
 };
 
 const getZookeepers = () => {
+  let queryUrl = '/api/zookeepers?';
+
+  Object.entries(formData).forEach(([key, value]) => {
+    queryUrl += `${key}=${value}&`;
+  });
+
   fetch('/api/zookeepers')
     .then(response => {
       if (!response.ok) {
@@ -33,5 +40,20 @@ const getZookeepers = () => {
       printResults(zookeeperArr);
     });
 };
+
+const handleGetZookeepersSubmit = event => {
+  event.preventDefault();
+  const nameHTML = $zookeeperForm.querySelector('[name="name"]');
+  const name = nameHTML.value;
+
+  const ageHTML = $zookeeperForm.querySelector('[name="age"]');
+  const age = ageHTML.value;
+
+  const zookeeperObject = { name, age };
+
+  getZookeepers(zookeeperObject);
+};
+
+$zookeeperForm.addEventListener('submit', handleGetZookeepersSubmit);
 
 getZookeepers();
